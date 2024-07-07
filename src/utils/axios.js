8a82ae15463515874fa4841fa11b5d588a5e13bf
ramez.salman <ramez.salman@onestream.live>
2024-07-08 00:19:36 +0500

    Added logs to check on API error

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,11 +3,15 @@ import axios from 'axios';
 import { HOST_API_KEY } from '../config';
 // ----------------------------------------------------------------------
 
+console.log("Axios Base URL:", process.env.NEXT_PUBLIC_HOST_API);
+
 const axiosInstance = axios.create({ baseURL: HOST_API_KEY });
 
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
 );
+
+console.log("Axios instance -> ", axiosInstance);
 
 export default axiosInstance;
